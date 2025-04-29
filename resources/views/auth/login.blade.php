43767f5b5797dbbb36e786b5e7f3c05300ec1c5d
Alireza Talebi <at@imagearts.de>
2025-04-29 14:38:15 +0200

    - modified guest.blade, login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('general.email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('general.password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -28,19 +28,19 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <x-checkbox-input id="remember_me" name="remember"/>
-                <span class="ms-2 text-sm text-white">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-white">{{ __('general.remember_me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-white hover:text-custom_yellow rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom_yellow" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('general.forgot_password') }}
                 </a>
             @endif
 
-            <a class="underline text-sm text-white ml-4 hover:text-custom_yellow rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom_yellow" href="{{ route('password.request') }}">
-                {{ __('Log in') }}
+            <a class="underline text-sm text-white {{app()->isLocale('fa') ? 'mr-4' : 'ml-4' }} hover:text-custom_yellow rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom_yellow" href="{{ route('password.request') }}">
+                {{ __('general.login') }}
             </a>
         </div>
     </form>
