8170e01c3eda63d9f3d56b2648cdf8f1cb8cdc66
Alireza Talebi <at@imagearts.de>
2025-04-29 14:37:15 +0200

    - added new lang files

---
+++ lang/en/general.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'login' =>'Login',
+    'logout' =>'Logout',
+    'register' =>'Register',
+    'home' =>'Home',
+    'profile' =>'Profile',
+    'password' =>'Password',
+    'email' =>'Email',
+    'remember_me' =>'Remember me',
+    'forgot_password' =>'Forgot your password',
+    'reset_password' =>'Reset password',
+];
---
+++ lang/fa/general.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'login' => 'ورود',
+    'logout' => 'خروج',
+    'register' => 'ثبت نام',
+    'home' => 'خانه',
+    'profile' => 'پروفایل',
+    'password' => 'رمز عبور',
+    'email' => 'ایمیل',
+    'remember_me' => 'مرا به خاطر بسپار',
+    'forgot_password' => 'رمز عبور خود را فراموش کرده‌اید؟',
+    'reset_password' => 'بازنشانی رمز عبور',
+];
