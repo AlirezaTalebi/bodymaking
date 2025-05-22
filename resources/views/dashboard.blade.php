d4e58a78ea19d8c930850e1f7ad5a1d00c918f9a
Alireza Talebi <at@imagearts.de>
2025-05-22 11:52:55 +0200

    Add Livewire calendar component to dashboard
    Replaced the static login message with a Livewire calendar component for dynamic content on the dashboard. Integrated Livewire styles and scripts into the main layout to support the new component.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-gradient-to-br from-zinc-900 to-gray-800 text-white shadow-xl sm:rounded-lg p-6">
-                <p class="text-lg font-medium text-gray-300">
-                    {{ __("You're logged in!") }}
-                </p>
+               <livewire:calendar/>
             </div>
         </div>
     </div>
