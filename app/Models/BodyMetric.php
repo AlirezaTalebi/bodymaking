f71fbf2a152ac2b0a83e09a38ef9a9b99de2edcf
Alireza Talebi <at@imagearts.de>
2025-05-26 09:54:30 +0200

    Add migrations, models, controllers, and factories for core entities

---
+++ app/Models/BodyMetric.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BodyMetric extends Model
+{
+    /** @use HasFactory<\Database\Factories\BodyMetricFactory> */
+    use HasFactory;
+}
